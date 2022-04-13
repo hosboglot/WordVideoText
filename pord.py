3087774398764e798ba5c0a46483b0484584fb2f
Timur Galeev <templay3000@gmail.com>
2022-04-13 15:00:10 +0400

    Обернул все в try

--- pord.py
+++ pord.py
@@ -7,13 +7,16 @@ from MyLongPoll import MyVkLongPoll
 from vk_api.bot_longpoll import VkBotEventType
 from main import write_text
 
-with open("token.txt", 'r') as tk:
-    token = tk.read()
+try:
+    with open("token.txt", 'r') as tk:
+        token = tk.read()
 
-vk_session = vk_api.VkApi(token=token)
-vk = vk_session.get_api()
-upload = VkUpload(vk_session)
-longpoll = MyVkLongPoll(vk_session, "212535184")
+    vk_session = vk_api.VkApi(token=token)
+    vk = vk_session.get_api()
+    upload = VkUpload(vk_session)
+    longpoll = MyVkLongPoll(vk_session, "212535184")
+except Exception as e:
+    print(e)
 
 
 def send_msg(msg, att=''):
@@ -27,9 +30,11 @@ def send_msg(msg, att=''):
     except Exception as e:
         print(e)
 
-
-for event in longpoll.listen():
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        write_text(event.object['message']['text'], 3.5)
-        gif = upload.document_message(doc=open('output.gif', 'rb'), peer_id=event.object['message']['peer_id'])
-        send_msg("", f"doc{gif['doc']['owner_id']}_{gif['doc']['id']}")
+try:
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            write_text(event.object['message']['text'], 3.5)
+            gif = upload.document_message(doc=open('output.gif', 'rb'), peer_id=event.object['message']['peer_id'])
+            send_msg("", f"doc{gif['doc']['owner_id']}_{gif['doc']['id']}")
+except Exception as e:
+    print(e)
